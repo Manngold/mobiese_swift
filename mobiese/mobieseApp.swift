ccb002c1574c11f142df339c32cc07d0db5b1d22
Manngold <jsw4820@gmail.com>
2021-12-31 20:38:08 +0900

    Initial Commit

---
+++ mobiese/mobieseApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobieseApp.swift
+//  mobiese
+//
+//  Created by Shaashop on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct mobieseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
